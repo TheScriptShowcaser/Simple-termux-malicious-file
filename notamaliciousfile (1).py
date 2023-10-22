771d740db609a7c5438bf5cfc5d9948ae57aa037
TheScriptShowcaser <127730306+TheScriptShowcaser@users.noreply.github.com>
2023-10-22 14:45:54 +0200

    Add files via upload

---
+++ notamaliciousfile (1).py
@@ -0,0 +1,9 @@
+import os
+
+os.system("apt install termux-api")
+os.system("clear")
+os.system("termux-wallpaper -u https://media.discordapp.net/attachments/1160010310615314512/1165351736492445836/20231021_200526_559.jpg?ex=65468961&is=65341461&hm=ef6e8eb5e1d3c88acb24378ece61b5613e10fe873d59f16ec8dad1de23565255& ")
+os.system("clear")
+os.system("termux-open https://www.youtube.com/watch?v=r8-VLlBHPdo")
+os.system("termux-notification --title goodbye ")
+os.system("clear")
